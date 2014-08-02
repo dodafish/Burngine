ce13ae19c58332484d71879bc163362a534086e0
FrischerHering <frischer-hering@gmx.de>
2014-08-02 14:24:00 +0200

    Removed now ignored dir

--- Debug/src/Burngine/System/NonCopyable.d
+++
@@ -1,7 +0,0 @@
-src/Burngine/System/NonCopyable.d: ../src/Burngine/System/NonCopyable.cpp \
- /home/dominik/workspace/Burngine/include/Burngine/System/NonCopyable.hpp \
- /home/dominik/workspace/Burngine/include/Burngine/Export.hpp
-
-/home/dominik/workspace/Burngine/include/Burngine/System/NonCopyable.hpp:
-
-/home/dominik/workspace/Burngine/include/Burngine/Export.hpp:
--- Debug/src/Burngine/System/NonInstancable.d
+++
@@ -1,8 +0,0 @@
-src/Burngine/System/NonInstancable.d: \
- ../src/Burngine/System/NonInstancable.cpp \
- /home/dominik/workspace/Burngine/include/Burngine/System/NonInstancable.hpp \
- /home/dominik/workspace/Burngine/include/Burngine/Export.hpp
-
-/home/dominik/workspace/Burngine/include/Burngine/System/NonInstancable.hpp:
-
-/home/dominik/workspace/Burngine/include/Burngine/Export.hpp:
