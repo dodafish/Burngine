633cfe3eb4e1459e057a7474d2f234f8a38e5ccd
FrischerHering <frischer-hering@gmx.de>
2014-08-23 13:52:02 +0200

    Implemented error logger with execution abort

---
+++ include/Burngine/System/Error.hpp
@@ -0,0 +1,57 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Burngine is distributed under the GNU LGPL v3 License
+// ====================================================
+//
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation;
+//    version 3.0 of the License
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef ERR_HPP_
+#define ERR_HPP_
+
+#include <Burngine/Export.hpp>
+#include <Burngine/System/NonInstancable.hpp>
+#include <string>
+
+namespace burn {
+
+/**
+ * @brief Error logging. Optimized for platform, i.e. using message boxes.
+ */
+class BURNGINE_API_EXPORT Error : public NonInstancable {
+public:
+
+	/**
+	 * @brief Log an error string
+	 *
+	 * @param msg Error message
+	 */
+	static void log(const std::string& msg);
+
+};
+
+/////////////////////////////////////////////////////////////////////////////
+// Shortcut:
+inline void err(const std::string& msg) {
+	Error::log(msg);
+}
+
+} /* namespace burn */
+
+#endif /* ERR_HPP_ */
---
+++ src/Burngine/System/Error.cpp
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Burngine is distributed under the GNU LGPL v3 License
+// ====================================================
+//
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation;
+//    version 3.0 of the License
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#include <Burngine/System/Error.hpp>
+#include <iostream>
+
+void msgBox(const std::string& msg);
+
+#if defined(BURNGINE_OS_WINDOWS)
+
+#include <windows.h>
+void msgBox(const std::string& msg) {
+	MessageBox(NULL, msg.c_str(), "ERROR", MB_ICONERROR | MB_OK);
+}
+
+#else
+
+void msgBox(const std::string& msg){}
+
+#endif
+
+namespace burn {
+
+void Error::log(const std::string& msg) {
+
+	msg += "\n";
+
+	// Also print the error into console
+	std::cerr << msg;
+
+	msgBox(msg);
+
+	// Interrupt execution
+	exit(8074);
+
+}
+
+} /* namespace burn */
