d5192e5a1ae010d2a00099330c66d92f3ebaebea
FrischerHering <frischer-hering@gmx.de>
2014-08-30 12:07:39 +0200

    Added GuiNode and Rectangle; no impl.

---
+++ include/Burngine/Graphics/Gui/Rectangle.hpp
@@ -0,0 +1,44 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Burngine is distributed under the GNU LGPL v3 License
+// ====================================================
+//
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation;
+//    version 3.0 of the License
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef RECTANGLE_HPP_
+#define RECTANGLE_HPP_
+
+#include <Burngine/Export.hpp>
+#include <Burngine/Graphics/Gui/GuiNode.hpp>
+
+namespace burn {
+
+	class BURNGINE_API_EXPORT Rectangle : public GuiNode {
+	public:
+
+		virtual void render();
+
+	private:
+
+	};
+
+} /* namespace burn */
+
+#endif /* RECTANGLE_HPP_ */
