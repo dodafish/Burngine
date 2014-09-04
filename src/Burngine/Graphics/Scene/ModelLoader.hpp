0e386bb2826ae4a0ce0195ada1e3662025103a20
FrischerHering <frischer-hering@gmx.de>
2014-09-04 13:32:11 +0200

    Added class ModelLoader

---
+++ src/Burngine/Graphics/Scene/ModelLoader.hpp
@@ -0,0 +1,43 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Burngine is distributed under the GNU LGPL v3 License
+// ====================================================
+//
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation;
+//    version 3.0 of the License
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef MODELLOADER_HPP_
+#define MODELLOADER_HPP_
+
+#include <Burngine/Export.hpp>
+
+namespace burn {
+	namespace priv {
+
+		class BURNGINE_API_EXPORT ModelLoader {
+		public:
+
+		private:
+
+		};
+
+	} /* namespace priv */
+} /* namespace burn */
+
+#endif /* MODELLOADER_HPP_ */
