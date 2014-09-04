a5a20e4e02e5720ea84236d1ea6df4caa5b47b98
FrischerHering <frischer-hering@gmx.de>
2014-09-04 12:58:56 +0200

    Added class Camera with aspect ratio

---
+++ include/Burngine/Graphics/Scene/Camera.hpp
@@ -0,0 +1,59 @@
+//////////////////////////////////////////////////////////////////////////////
+//
+// Burngine is distributed under the GNU LGPL v3 License
+// ====================================================
+//
+//    Copyright (C) 2014 Dominik David (frischer-hering@gmx.de)
+//
+//    This library is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation;
+//    version 3.0 of the License
+//
+//    This library is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public
+//    License along with this library; if not, write to the Free Software
+//    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+//    USA
+//
+//////////////////////////////////////////////////////////////////////////////
+
+#ifndef CAMERA_HPP_
+#define CAMERA_HPP_
+
+#include <Burngine/Export.hpp>
+#include <Burngine/Graphics/Scene/Transformable3D.hpp>
+
+namespace burn {
+
+	/**
+	 * @brief The viewpoint for rendering scenes
+	 */
+	class BURNGINE_API_EXPORT Camera : public Transformable3D {
+	public:
+
+		/**
+		 * @brief Set the aspect ratio
+		 *
+		 * @param ratio The aspect ratio (e.g. 1.6f for 16:10 aspect)
+		 */
+		void setAspectRatio(const float& ratio);
+
+		/**
+		 * @brief Get the aspect ratio
+		 *
+		 * @return Camera's aspect ratio
+		 */
+		const float& getAspectRatio() const;
+
+	private:
+		float m_aspectRatio;
+	};
+
+} /* namespace burn */
+
+#endif /* CAMERA_HPP_ */
